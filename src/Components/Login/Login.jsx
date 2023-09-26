8213f9858a47e7fbda4082dcbca1e862632acae9
kdrbipul <kdrbipul@gmail.com>
2023-09-26 18:00:42 +0600

    last commit

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            this is Login
-        </div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
     );
 };
 
